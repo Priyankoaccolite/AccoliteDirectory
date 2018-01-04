f1a55317bd75ef0ac55fbc801338b247f8b357b2
mm117 <mukeshmishra117@gmail.com>
2018-01-04 14:52:53 +0530

    Admin Page incorporated

--- js/services/dataService.js
+++ js/services/dataService.js
@@ -3,7 +3,7 @@ app.service("dataService",["$http","$q",function($http,$q){
 
     this.getData=function(){
         var defered=$q.defer();
-        $http.get('../../sample.json').then(function(data){
+        $http.get('sample.json').then(function(data){
             defered.resolve(data);
         },function(error){
             defered.reject(error); 
@@ -13,3 +13,31 @@ app.service("dataService",["$http","$q",function($http,$q){
     
     
 }]);
+
+
+app.service("lookupService",["$http","$q",function($http,$q){
+    
+    this.getAllLookups=function(){
+          var defered=$q.defer();
+        $http.get('http://localhost:8080/accolite/hrms/reference/getAllLookups').then(function(data){
+            defered.resolve(data);
+        },function(error){
+            defered.reject(error); 
+        });
+        return defered.promise;
+    };
+    this.filterLookups=function(lookups,lookupType){
+        var specificLookup=[];
+        
+        lookups.map(function(ele,index,lookupArray){
+            if(ele.lookupGroup==lookupType){
+               specificLookup.push(ele); 
+            }
+        })
+        
+        return specificLookup;
+    }
+}
+    
+    
+])
